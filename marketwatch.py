c82e4503c8f2554f38139453708486266e91d948
gigimic <gigimic@gmail.com>
2019-08-14 11:32:57 +0530

    modified the parser

--- marketwatch.py
+++ marketwatch.py
@@ -1,21 +1,12 @@
 import feedparser
-import spacy
-import re
-import nltk
-import urllib
 from bs4 import BeautifulSoup
-
-nlp_en = spacy.load('en')
 
 NewsFeed1 = feedparser.parse("http://www.moneycontrol.com/rss/business.xml")
 print('Number of RSS posts :', len(NewsFeed1.entries))
 
-story = NewsFeed1.entries
-
-for x in range(len(NewsFeed1.entries)):
-    print("\n", story[x].title)
-    html_marked = story[x].summary
+for entry in NewsFeed1.entries:
+    print("\n", entry.title)
+    html_marked = entry.summary
     soup = BeautifulSoup(html_marked, "lxml")
     storySummary = soup.get_text()
     print(storySummary)
-
